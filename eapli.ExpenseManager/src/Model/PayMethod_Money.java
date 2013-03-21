aa2c4c9705cfbe82bf01a90ecb808ebf3097067b
BDani <1110829@isep.ipp.pt>
2013-03-21 15:47:33 +0000

    PayMethod_Money : Done; UI para pagamento em dinheiro pronto, basta acrescentar restantes opções

---
+++ eapli.ExpenseManager/src/Model/PayMethod_Money.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author BDani
+ */
+public class PayMethod_Money extends PayMethod {
+    
+    public PayMethod_Money() {
+        super("Dinheiro");
+    }
+    
+}
