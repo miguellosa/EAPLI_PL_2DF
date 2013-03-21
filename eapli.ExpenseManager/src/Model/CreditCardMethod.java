b79b77da42364b1e750e0d4c8efba496ecb69173
i101008 <i101008@PCI078.dei.isep.ipp.pt>
2013-03-21 15:36:26 +0000

    CreditCardMethod Class: Constructor + Getters

--- eapli.ExpenseManager/src/Model/CreditCardMethod.java
+++ eapli.ExpenseManager/src/Model/CreditCardMethod.java
@@ -1,13 +1,43 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package Model;
-
+import eapli.util.DateTime;
 /**
  *
  * @author i101008, i101516, i090589
  */
 public class CreditCardMethod extends PayMethod {
+    
+    private String creditCardNumber;
+    private String ownerName; 
+    private DateTime expiringDate;
+    private DateTime creationDate;
+    private String cardSecurityCode; //CCV
+    
+    public CreditCardMethod(String ownerName,String creditCardNumber, DateTime creationDate, DateTime expiringDate, String cardSecurityCode){
+        super(ownerName);
+        this.creditCardNumber=creditCardNumber;
+        this.expiringDate=expiringDate;
+        this.cardSecurityCode=cardSecurityCode;
+        this.creationDate=creationDate;
+    }
+    
+    public String getCreditCardNumber() {
+        return creditCardNumber;
+    }   
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public DateTime getExpiringDate() {
+        return expiringDate;
+    }
+
+    public DateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public String getCardSecurityCode() {
+        return cardSecurityCode;
+    }
     
 }
