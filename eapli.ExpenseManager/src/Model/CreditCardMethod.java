5cfea8bc1aad202d6bdaa65b81513ae5fc3b73f7
i101008 <i101008@PCI078.dei.isep.ipp.pt>
2013-03-21 15:11:07 +0000

    CreditCardMethod : Not Done

---
+++ eapli.ExpenseManager/src/Model/CreditCardMethod.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author i101008, i101516, i090589
+ */
+public class CreditCardMethod extends PayMethod {
+    
+}
