0fb3d2d909b64f96607a4d2ae4282554f9d10ec4
i101008 <i101008@PCI078.dei.isep.ipp.pt>
2013-03-21 15:03:58 +0000

    PayMethod(Super Class) : Done

---
+++ eapli.ExpenseManager/src/Model/PayMethod.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author i101008, i101516, i090589
+ */
+public abstract class PayMethod {
+    
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+}
